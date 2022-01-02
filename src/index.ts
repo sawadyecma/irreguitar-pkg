9902967b6be9f6c9bdb2f6bb13c37f697633f1b7
sawadyecma <snowslidez@yahoo.co.jp>
2022-01-02 20:53:41 +0900

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./chord/chordAnalyzer";
+export * from "./chord/chordNamer";
+export * from "./chord/chordParser";
+export * from "./sound/absnote";
+export * from "./sound/absnotes";
+export * from "./sound/chord";
