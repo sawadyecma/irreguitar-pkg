63cf0dd1665394ed0a8bcf457b355cda00060eee
sawadyecma <snowslidez@yahoo.co.jp>
2022-01-02 14:29:18 +0900

    add ChordParserImpl

---
+++ src/chord/chordParser.ts
@@ -0,0 +1,21 @@
+import { Chord } from "../sound/interfaces";
+import { ChordAnalyzer, ChordNamer, ChordParser } from "./interfaces";
+
+export class ChordParserImpl implements ChordParser{
+
+    private analyzer: ChordAnalyzer
+    private namer: ChordNamer
+    
+    constructor(
+        analyzer: ChordAnalyzer,
+        namer: ChordNamer,
+    ){
+        this.analyzer = analyzer
+        this.namer = namer
+    }
+
+    parse(chord: Chord): string {
+        const analyzed = this.analyzer.analyze(chord)
+        return this.namer.name(analyzed)
+    }
+}
---
+++ src/chord/interfaces.ts
@@ -0,0 +1,21 @@
+import { Chord } from "../sound/interfaces";
+
+export interface ChordParser {
+	parse(chord: Chord): string
+}
+
+export interface ChordAnalyzer{
+	analyze(chord: Chord):AnalyzedChord
+}
+
+export interface ChordNamer{
+	name(chord: AnalyzedChord):string
+}
+
+// 長調か短調か
+export type Lentype = "None" | "Major" | "Minor" | "Invalid"
+
+export type AnalyzedChord = {
+    thirdType: Lentype
+    seventhType: Lentype
+}
